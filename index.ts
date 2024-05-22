a6f285fbc345108e2cfce45f4327f17ff55a3d85
Abdullah Saleem <abdullahwork321@gmail.com>
2024-05-22 20:28:53 +0500

    first version uploaded

---
+++ index.ts
@@ -0,0 +1,27 @@
+
+
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+
+while (condition) {
+let taskadd = await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"what you want to add in your list?"
+        },
+        {
+            name:"addmore",
+            type:"confirm",
+            message:"Do you want to add more?"
+        }
+    ]
+)
+todos.push(taskadd.todo);
+condition = taskadd.addmore
+console.log(todos);
+
+}
